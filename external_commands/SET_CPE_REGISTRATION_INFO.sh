215a435fd652676c51c54f335537dc18182ed590
Ignacio Rojo <ignacio.rojo.sebastian@gmail.com>
2016-01-18 17:36:26 +0100

    tests++

--- external_commands/SET_CPE_REGISTRATION_INFO.sh
+++ external_commands/SET_CPE_REGISTRATION_INFO.sh
@@ -10,3 +10,13 @@ commandfile='/var/lib/shinken/nagios.cmd'
 printf "[%lu] SET_CPE_REGISTRATION_INFO;2;olt;3:7;1;{'up': 456}" $now > $commandfile
 
 printf "[%lu] PROCESS_HOST_CHECK_RESULT;cpe10;0;demo" $now > $commandfile
+
+
+printf "[%lu] PROCESS_SERVICE_CHECK_RESULT;localhost;memory;0;MEMORY" $now > $commandfile
+
+printf "[%lu] PROCESS_HOST_CHECK_RESULT;cpe5;0;'active' olthuawei 0/0/0-0 no IP | txoctets=0B rxoctets=0B" $now > $commandfile
+
+
+
+/opt/fos/bin/push_dhcp_lease --transaction=commit --lease-time=3600 --leased-address=10.142.0.44 --hardware=30:c:23:be:3b:3b --host-name=atadevice30:0c:23:be:3b:3b --circuit-id=pon/422/OLTZTE/1/1/2/5/6/1 --remote-id=5a544547c15383f2
+
